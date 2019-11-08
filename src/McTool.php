7553fe7ef61a57a1508f331b1b8f31226cc433d8
王明昌 <287851074@qq.com>
2019-11-08 19:54:28 +0800

    测试

--- src/McTool.php
+++ src/McTool.php
@@ -7,13 +7,13 @@ class Mctool
     public function __construct(){
       //使用记录
 // include './function.php';
-        include './vendor/wmc1125/mctoolsdk/src/tool/System.php';
-        dd(\Wmc1125\Mctoolsdk\System::get_info());
+//        include './vendor/wmc1125/mctoolsdk/src/tool/System.php';
+//        dd(\Wmc1125\Mctoolsdk\System::get_info());
     
     }
     public function index(){
         
-        return "mctool";
+        echo  "mctool";
     }
      
 
