747c5745fc589906ec17c0bf0b0bf911dc076c8d
王明昌 <287851074@qq.com>
2019-11-08 21:19:01 +0800

    测试

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require 'vendor/autoload.php';
+
+//$auth = new Wmc1125\Mctoolsdk\IndexController();
+//$auth->index();
+
+$auth2 = new Wmc1125\Mctoolsdk\Mctool();
+$auth2->index();
---
+++ src/IndexController.php
@@ -0,0 +1,9 @@
+<?php
+namespace Wmc1125\Mctoolsdk;
+
+class IndexController{
+
+    public function index(){
+        echo 'index';
+    }
+}
